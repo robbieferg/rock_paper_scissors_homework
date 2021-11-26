489bcccb871a43b50ec7ebd3ccb8b94fe6e8f524
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-11-26 19:46:48 +0000

    MVP completed

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,8 +6,9 @@ from models.player import Player
 
 @app.route("/<player_1_choice>/<player_2_choice>/")
 def display_result(player_1_choice, player_2_choice):
-    player_1 = Player("Player 1, player_1_choice")
+    
+    player_1 = Player("Player 1", player_1_choice)
     player_2 = Player("Player 2", player_2_choice)
     game_1 = Game(player_1, player_2)
     winner = game_1.play_game()
-    return render_template("result", title = "Result", winner = winner)
+    return render_template("result.html", title = "Result", winner = winner)
