92467e9fb477bd35e75caf12f39229403f74a3ed
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-11-28 08:44:07 +0000

    All tasks and extensions complete bar CSS styling. Playing against the computer now works.

--- controllers/controller.py
+++ controllers/controller.py
@@ -8,7 +8,7 @@ from models.player import Player
 def welcome():
     return render_template("welcome.html", title = "Welcome")
 
-@app.route("/<player_1_choice>/<player_2_choice>/")
+@app.route("/<player_1_choice>/<player_2_choice>")
 def display_result(player_1_choice, player_2_choice):
     player_1 = Player("Player 1", player_1_choice)
     player_2 = Player("Player 2", player_2_choice)
@@ -16,14 +16,14 @@ def display_result(player_1_choice, player_2_choice):
     winner = game_1.play_game(player_1, player_2)
     return render_template("result.html", title = "Result", winner = winner, player_1 = player_1, player_2 = player_2)
 
-@app.route("/play/")
+@app.route("/play")
 def play():
     return render_template("play.html", title = "play")
 
-@app.route("/play/", methods = ["POST"])
+@app.route("/play", methods = ["POST"])
 def play_computer_result():
     new_game = Game()
-    player_1 = Player(request.form["name"], request.form["choice"])
-    player_2 = new_game.generate_computer_player
-    winner = new_game.play_game(player_1, player_2)
-    return render_template("result.html", title = "Result", winner = winner, player_1 = player_1, player_2 = player_2)
+    new_player_1 = Player(request.form["name"], request.form["choice"])
+    new_player_2 = new_game.generate_computer_player()
+    winner = new_game.play_game(new_player_1, new_player_2)
+    return render_template("result.html", title = "Result", winner = winner, player_1 = new_player_1, player_2 = new_player_2)
--- tests/game_test.py
+++ tests/game_test.py
@@ -7,17 +7,23 @@ class TestGame(unittest.TestCase):
     def test_game_has_winner(self):
         self.player_1 = Player("Player 1", "paper")
         self.player_2 = Player("Player 2", "scissors")
-        self.game_1 = Game(self.player_1, self.player_2)
+        self.game_1 = Game()
 
         expected = "Player 2"
-        actual = self.game_1.play_game()
+        actual = self.game_1.play_game(self.player_1, self.player_2).name
 
         self.assertEqual(expected, actual)
 
     def test_game_is_draw(self):
         self.player_1 = Player("Player 1", "rock")
         self.player_2 = Player("Player 2", "rock")
-        self.game_2 = Game(self.player_1, self.player_2)
+        self.game_2 = Game()
 
         expected = None
-        actual = self.game_2.play_game()
+        actual = self.game_2.play_game(self.player_1, self.player_2)
+
+    def test_generate_computer_player(self):
+        game = Game()
+        self.new_computer_player = game.generate_computer_player()
+        self.assertEqual("The Computer", self.new_computer_player.name)
+        self.assertEqual(True, self.new_computer_player.choice in ["rock", "paper", "scissors"])
